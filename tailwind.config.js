dff43280e719b38df54a665b8842d8ebd4a6916e
chaitanya <131352623+chaimass6@users.noreply.github.com>
2025-01-28 22:59:37 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} *
 export default {
   content: [],
   theme: {
@@ -6,4 +6,3 @@ export default {
   },
   plugins: [],
 }
-
